d5711c4cc5fefa2a3c26838043dfe2e6391196be
Isreal Christopher <isreal2017b@gmail.com>
2026-01-12 08:38:21 +0100

    ss

--- funiture/models.py
+++ funiture/models.py
@@ -75,7 +75,7 @@ class Product(models.Model):
     dimension = models.CharField(max_length=100, blank=True, null=True)
     care = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    height = models.PositiveIntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.name
