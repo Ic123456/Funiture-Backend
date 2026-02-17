74f752192f8224c629a968054bcf5f4cfc0f4e69
Isreal Christopher <isreal2017b@gmail.com>
2026-02-17 22:14:59 +0100

    Initial commit

--- core/settings.py
+++ core/settings.py
@@ -94,17 +94,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-if not DEBUG:
+if  DEBUG:
     DATABASES = {
         "default": dj_database_url.parse(config("DATABASE_URL"))
     }
 else:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
